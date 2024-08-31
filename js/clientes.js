cebd6065dd4bf950898c0a0cf3f22d499a78eea5
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2024-08-31 16:15:15 -0600

    ajuste en la pagina de historial de prestamo

--- js/clientes.js
+++ js/clientes.js
@@ -1,3 +1,13 @@
 $(document).on("click", "#btnPerfilCLiente", function () {
   window.location = "perfil-cliente.html";
 });
+
+/* -------------------------------------------------------------------------- */
+/*                       tablas responsivas de prestamos                      */
+/* -------------------------------------------------------------------------- */
+
+// $("#myTablePrestamo").DataTable({
+//   responsive: true,
+// });
+
+/* --------------------- tablas responsivas de prestamos -------------------- */
