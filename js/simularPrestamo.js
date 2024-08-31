426c45c002afd554a9dc88fee9a30c4bc192d64f
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2024-08-30 21:50:27 -0600

    alerta en simular prestamo, organizar tablas de inicio, pagina de mantenimiento

--- js/simularPrestamo.js
+++ js/simularPrestamo.js
@@ -201,6 +201,23 @@ $("#btnCalcularCuota").on("click", function () {
 
     /* ------------------------- MOSTRAMOS LOS DETALLES ------------------------- */
   } else {
-    console.log("INGRESE LOS DATOS REQUERIDOS");
+    toastr.options = {
+      closeButton: true,
+      newestOnTop: false,
+      progressBar: true,
+      positionClass: "toast-top-right",
+      preventDuplicates: false,
+      onclick: null,
+      showDuration: "300",
+      hideDuration: "1000",
+      timeOut: "5000",
+      extendedTimeOut: "1000",
+      showEasing: "swing",
+      hideEasing: "linear",
+      showMethod: "fadeIn",
+      hideMethod: "fadeOut",
+    };
+
+    toastr.error("ERROR: Todos los campos son obligatorios");
   }
 });
